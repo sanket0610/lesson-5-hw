2c9f17b4de41dc66fa37fb9c7be36e4492f2c0b9
sanket0610 <sanketkingz10@gmail.com>
2025-07-15 17:28:50 +0530

    Add files via upload

---
+++ hw.py
@@ -0,0 +1,14 @@
+print("Start with a capital letter")
+w=input("Enter Season")
+s="Summer"
+wi="Winter"
+m="Monsoon"
+if w==s:
+    print("Wear Cotton Clothes")
+elif w==wi:
+    print("Wear Woolen Clothes")
+elif w==m:
+    print("Wear a Raincoat")
+else:
+    print("Not a season")
+
